b32ced3816b411a1ccd55bdfb1c5c7b852ad1855
Matt Brown <mattdbrown7@gmail.com>
2018-09-08 18:23:13 -0500

    remove unnecessary gulp and webpack, fixed debug issues in vscode, created workspace, got file to open, parsed file but wrong parsing.

--- src/FileProcessor/LibraryFileProcessor.ts
+++ src/FileProcessor/LibraryFileProcessor.ts
@@ -1,27 +1,29 @@
 import fs from 'fs';
+import { Dictionary } from 'lodash';
+//import xml2js from 'xml2js';
+const parseXmlStringToJson = require('xml2js').parseString;
 
 export class LibraryFileProcessor {
-    readonly OLD_FILE_PATH: string = "/CompareFiles/old";
-    readonly NEW_FILE_PATH: string = "/CompareFiles/new";
+    readonly OLD_FILE_PATH: string = `E://Workspaces/iTunesCompareLibrary/CompareFiles/old/Library.xml`;
+    readonly NEW_FILE_PATH: string = `E://Workspaces/iTunesCompareLibrary/CompareFiles/new/Library.xml`;
 
     constructor(){}
 
     public async compareLibraries (): Promise<void> {
+        const oldLibrary: string = await this.getFileData(this.OLD_FILE_PATH);
+        const newLibrary: string = await this.getFileData(this.NEW_FILE_PATH);
+        const oldLibraryXml: any = await this.convertXmlStringToJson(oldLibrary);
+        const newLibraryXml: any = await this.convertXmlStringToJson(newLibrary);
+        let asdf = newLibraryXml.plist.dict[0].dict[0].dict[0];
         console.log('asdf');
-        //const oldLibrary = await this.getFileData(this.OLD_FILE_PATH);
-        //const newLibrary = await this.getFileData(this.NEW_FILE_PATH);
-        //const oldLibraryXml = (new DOMParser()).parseFromString(oldLibrary, "text/xml");
-        //const newLibraryXml = (new DOMParser()).parseFromString(newLibrary, "text/xml");
-
-        //const 
     }
 
     private getFileData (filePath: string): Promise<string> {
         return new Promise<string>((resolve) => {
-            fs.readFile(filePath, 'r', function(err, fd){
+            fs.readFile(filePath, 'utf8', function(err: any, fd: any){
                 if (err) {
                     if (err.code === 'ENOENT') {
-                        console.error('myfile does not exist');
+                        console.error('my file does not exist');
                         return;
                     }
                     throw err;
@@ -30,12 +32,19 @@ export class LibraryFileProcessor {
             })
         });
     }
-
-    private convertXmlToJson (xml: XMLDocument){
-
+    // todo: this parses the data wrong!  this needs to actually work in a standard repeatable way.
+    private async convertXmlStringToJson (xmlString: string): Promise<JSON>{
+        return new Promise<JSON>((resolve) => {
+            parseXmlStringToJson(xmlString, function(err: any, result: JSON) {
+                if(err){
+                    console.error(err);
+                }
+                resolve(result);
+            });
+        });
     }
 
-    private processFile (fileData: string, type: any) {
+    private convert (fileData: string, type: any) {
 
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-//import * as LibraryFileProcessor from "./FileProcessor/LibraryFileProcessor";
+import { LibraryFileProcessor } from "./FileProcessor/LibraryFileProcessor";
 
 export class Startup {
     public static main(): number {
-        //const libraryFileProcessor = new LibraryFileProcessor();
-        //libraryFileProcessor.compareLibraries();
-        console.log('asdf');
+        const libraryFileProcessor = new LibraryFileProcessor();
+        libraryFileProcessor.compareLibraries();
         return 0;
     }
 }
