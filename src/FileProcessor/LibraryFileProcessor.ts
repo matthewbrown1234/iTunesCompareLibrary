ea7dc76bcd05102dbefb74804fc459396a5b027d
Matt Brown <mattdbrown7@gmail.com>
2018-09-09 12:55:01 -0500

    got library into manageable Xml documents.

--- src/FileProcessor/LibraryFileProcessor.ts
+++ src/FileProcessor/LibraryFileProcessor.ts
@@ -1,7 +1,6 @@
 import fs from 'fs';
-import { Dictionary } from 'lodash';
-//import xml2js from 'xml2js';
-const parseXmlStringToJson = require('xml2js').parseString;
+const DOMParser = require('xmldom').DOMParser;
+import xpath from 'xpath';
 
 export class LibraryFileProcessor {
     readonly OLD_FILE_PATH: string = `E://Workspaces/iTunesCompareLibrary/CompareFiles/old/Library.xml`;
@@ -10,12 +9,17 @@ export class LibraryFileProcessor {
     constructor(){}
 
     public async compareLibraries (): Promise<void> {
-        const oldLibrary: string = await this.getFileData(this.OLD_FILE_PATH);
+        //const oldLibrary: string = await this.getFileData(this.OLD_FILE_PATH);
         const newLibrary: string = await this.getFileData(this.NEW_FILE_PATH);
-        const oldLibraryXml: any = await this.convertXmlStringToJson(oldLibrary);
-        const newLibraryXml: any = await this.convertXmlStringToJson(newLibrary);
-        let asdf = newLibraryXml.plist.dict[0].dict[0].dict[0];
+
+        const newLibraryXml: XMLDocument = this.convertStringToXmlDocument(newLibrary);
+        const newLibrarySongs: Array<XMLDocument> = this.getLibrarySongsFromXml(newLibraryXml);
+
         console.log('asdf');
+        for (var i = 0; i < newLibrarySongs.length; i++){
+            var key = newLibrarySongs[i];
+            //newLibrarySongs[i].childNodes[1].textContent
+        }
     }
 
     private getFileData (filePath: string): Promise<string> {
@@ -32,19 +36,12 @@ export class LibraryFileProcessor {
             })
         });
     }
-    // todo: this parses the data wrong!  this needs to actually work in a standard repeatable way.
-    private async convertXmlStringToJson (xmlString: string): Promise<JSON>{
-        return new Promise<JSON>((resolve) => {
-            parseXmlStringToJson(xmlString, function(err: any, result: JSON) {
-                if(err){
-                    console.error(err);
-                }
-                resolve(result);
-            });
-        });
+
+    private getLibrarySongsFromXml (xmlDocument: XMLDocument): Array<XMLDocument> {
+        return <Array<XMLDocument>>xpath.select(`//dict//dict//dict`, xmlDocument);
     }
 
-    private convert (fileData: string, type: any) {
-
+    private convertStringToXmlDocument (stringXml: string): XMLDocument {
+        return (new DOMParser()).parseFromString(stringXml, "text/xml")
     }
 }
